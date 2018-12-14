54b23f81f9f0f9683f94b814f216d0b53b3be203
Leonardo Lacerda <leonardolacerda@tce.al.gov.br>
2018-12-14 16:54:07 -0300

    Finalizando migração para redux
    Signed-off-by: Leonardo Lacerda <leonardolacerda@tce.al.gov.br>

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
+
+//Middlewares
+import promise from 'redux-promise'
+//Middleware Serve para disparar várias actions (não aguarda o fim das actions para executar as próximas)
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 import App from './main/app'
 import Reducers from './main/reducers'
 
-const store = createStore(Reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(thunk, multi, promise)(createStore)(Reducers, devTools)
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
